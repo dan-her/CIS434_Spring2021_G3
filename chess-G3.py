67d6dbe5ca85105ada1dad8c373e855e17f1061f
John Fedora <brukholevin@unknown68ecc56c72fd.attlocal.net>
2021-03-25 17:30:09 -0400

    experimental terminal-based chess game can be played, but be careful of typos

--- chess-G3.py
+++ chess-G3.py
@@ -1,11 +1,28 @@
 # !usr/bin/python
 import chess
 import tkinter as tk
+import sys
 
 def startup():
     # initialize the board
     board = chess.Board()
-    turn = 0
     print("Whee!")
+    while True:
+        move = input()
+        if (move == "exit" or move == "quit" or move == "q"):
+            sys.exit()
+        
+        if (chess.Move.from_uci(move) in board.legal_moves):
+            x = chess.Move.from_uci(move)
+            board.push(x)
+        else:
+            print("error: bad move")
+        
+        if (board.is_checkmate()):
+            print("g'over")
+            sys.exit()
+        
+        print(board)
+        
 
 startup();
