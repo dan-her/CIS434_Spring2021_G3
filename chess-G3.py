6709feabee151595a983a696968a3ae47e579b47
grant-glasser <g.m.glasser@vikes.csuohio.edu>
2021-03-26 15:09:35 -0400

    Add basic Piece class
    Needs PIL/Pillow library for the png images now

--- chess-G3.py
+++ chess-G3.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
 class ChessBoardGUI():
 	def __init__(self, root):
@@ -10,10 +11,9 @@ class ChessBoardGUI():
 		rowNum = 8 - (int(squareName[1]))
 		return self.squares[rowNum][colNum]
 
-
 # An individial square on a chess board
 class Square():
-	SquareLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] # each letter in an array for square AN names
+	SquareLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 	def __init__(self, root, x, y):
 		self.name = Square.getSquareName(x, y)
@@ -22,10 +22,9 @@ class Square():
 		if (x + y) % 2 == 0:
 			bgcolor = 'gray55'
 			txtcolor = 'black'
-
 		self.piece = 'none' # Eventually for occupying piece
 		self.canvas = tk.Canvas(root, bg = bgcolor, width=64, height=64, highlightthickness=0)  # Canvas with no border to represent each square
-		self.canvas.create_text(15,10,text=self.name, fill = txtcolor) # Square labeled with name of square
+		# self.canvas.create_text(15,10,text=self.name, fill = txtcolor)
 		self.canvas.grid(row = y, column = x) # Align to tkinter grid
 		self.canvas.bind('<Button-1>', lambda a: print(self.name + ' clicked')) # Print name of square when clicked
 
@@ -33,10 +32,32 @@ class Square():
 		col = Square.SquareLetters[x]
 		return col + str(8 - y)
 
-if __name__ == '__main__':
+	# Get grid x/y coords from a square
+	def getCoords(self):
+		colNum = Square.SquareLetters.index(self.name[0])
+		rowNum = 8 - (int(self.name[1]))
+		return colNum, rowNum
+
+class Piece():
+	def __init__(self,root, chessboard, pieceName, squareName):
+		path = "assets//"+pieceName + '.png'
+		image = Image.open(path)
+		image.thumbnail((48, 48)) # Resize the piece to fit (can also use image.resize I believe)
+		self.img = ImageTk.PhotoImage(image)
+		chessboard.getSquare(squareName).canvas.create_image(32,32,image=self.img)
+
+def InitWindow():
 	root = tk.Tk()
 	root.geometry('768x640')
-	chessboard = ChessBoardGUI(root)
+	root.title('Chess')
+	root.iconbitmap('assets//icon.ico')
+	root.configure(background='grey7')
 	root.bind('<Escape>', lambda e: root.destroy())
+	return root
+
+if __name__ == '__main__':
+	root = InitWindow()
+	chessboard = ChessBoardGUI(root)
+	piece = Piece(root, chessboard, 'knight_w', 'e4') # test adding a piece
 	root.mainloop()
 	print('Exiting...')
