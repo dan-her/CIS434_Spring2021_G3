624d728bcec89688eb7421e15dcffd29757ffc5a
grant-glasser <g.m.glasser@vikes.csuohio.edu>
2021-03-26 12:10:52 -0400

    Remove spacing between squares

--- chess-G3.py
+++ chess-G3.py
@@ -1,20 +1,18 @@
 import tkinter as tk
-import chess
-
 
 # An individial square on a chess board
 class Square():
 	def __init__(self, root, x, y):
 		self.index = x + y * 8
 
-		bgcolor = 'black'
+		bgcolor = 'gray13'
 		txtcolor = 'white'
 		if (x + y) % 2 == 0:
-			bgcolor = 'white'
+			bgcolor = 'gray55'
 			txtcolor = 'black'
 
 		self.piece = 'none' # Eventually for occupying piece
-		self.canvas = tk.Canvas(root, bg = bgcolor, width=25, height=25) 
+		self.canvas = tk.Canvas(root, bg = bgcolor, width=64, height=64, border=-2)  # Canvas with no border to represent each square
 		self.canvas.create_text(15,10,text=self.getSquareName(), fill = txtcolor) # Square labeled with name of square
 		self.canvas.grid(row = y, column = x) # Align to tkinter grid
 		self.canvas.bind('<Button-1>', lambda a: print(self.getSquareName() + ' clicked')) # Print name of square when clicked
@@ -51,12 +49,12 @@ def main():
 	root = tk.Tk()
 	for i in range(numrows):
 		for j in range(numcols):
-			root.grid_columnconfigure(j, weight=1)
-			root.grid_rowconfigure(i, weight=1)
 			Square(root,i,j)
-	# root.geometry(768x512)
+	root.geometry('768x640')
+	root.bind('<Escape>', lambda e: root.destroy())
 	root.mainloop()
 	root.destroy()
+
 
 if __name__ == '__main__':
 	main()
